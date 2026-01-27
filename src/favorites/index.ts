4d4f96340a62269e65a89f64fbfc198ba9e40a60
nhevers <faixzakhan10@gmail.com>
2026-01-27 12:34:37 +0100

    add favorites module

---
+++ src/favorites/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Favorites Module
+ */
+
+export { FavoritesStore, getFavoritesStore } from './FavoritesStore.js';
+export type { FavoriteItem, FavoritesStoreOptions } from './FavoritesStore.js';
+
+export { FavoritesManager, getFavoritesManager } from './FavoritesManager.js';
+export type { FavoriteEvent } from './FavoritesManager.js';
