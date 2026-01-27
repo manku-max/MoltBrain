2d0d224748b2ccb0e0178e3c8b8644c85425b781
nhevers <faixzakhan10@gmail.com>
2026-01-27 12:37:07 +0100

    add cache module

---
+++ src/cache/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Cache Module
+ */
+
+export { QueryCache } from './QueryCache.js';
+export type { CacheEntry, QueryCacheOptions } from './QueryCache.js';
+
+export { ContextCache, getContextCache } from './ContextCache.js';
+export type { ContextCacheEntry, ContextCacheOptions } from './ContextCache.js';
